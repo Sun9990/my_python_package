80d51674efdcb9eca3a3dcde1cfcfadd3a49ec04
Sun9990 <sunetradutta93@gmail.com>
2024-09-20 15:12:52 +0530

    setting up the package structure

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='my_python_package',
+    version='0.1.0',
+    description='A simple package that prints Hello, World!',
+    author='SDutta',
+    packages=find_packages(),
+    install_requires=[],
+)
+ 
